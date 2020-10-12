33cf7df600d9925afbb694453e2a06bf4b3332f7
LIManuel <leonmalisov@gmail.com>
2020-10-12 19:11:31 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component {
+  state = {
+    area: "",
+    result: ""
+  }
+
+  handleChange = (e) => {
+    this.setState({area: e.target.value})
+  }
+
+  handleSubmit = (e) => {
+   e.preventDefault()
+   let configObj = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      }
+    }
+    fetch(`http://localhost:3000/`, configObj)
+      .then(resp => resp.json())
+      .then((data) => {
+        this.setState({result: data.average_price})      
+      })
+      .catch(err => {
+        alert(err.message)
+      })
+  }
+
+  render() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>HOUSE SPIDER</h3>
+        
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" onChange={this.handleChange} value={this.state.area} />
+          <input type="submit" value="get data" />
+        </form>
+
+        <div>{this.state.result}</div>
       </header>
     </div>
   );
+  }
 }
 
 export default App;
